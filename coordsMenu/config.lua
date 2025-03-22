6bd9b0d9423d5d0ec3099cae8b0bdf003573126f
Mist the Goat <62227356+Mistextreme@users.noreply.github.com>
2025-03-22 17:01:02 +0000

    Update config.lua

--- coordsMenu/config.lua
+++ coordsMenu/config.lua
@@ -1,20 +1,20 @@
---- Config acces
+--- Config access
 
-Config_menuAcces = { "superadmin", "admin" } -- Modifiez les grades qui auront accès au menu via cette table.
+Config_menuAcces = { "superadmin", "admin" } -- Modify the ranks that will have access to the menu via this table.
 
---- Config du menu 
+--- Menu Config
 
-Config_titlemenu = "CoordsMenu" -- Titre du menu affiché dans la bannière.
-Config_subtile = "Intéraction" -- Sous-titre du menu.
-Config_TextureDictionary = "commonmenu" -- Nom du fichier YTD contenant vos assets.
-Config_TextureName = "gradient_bgd" -- Nom de l’asset dans le fichier.
+Config_titlemenu = "CoordsMenu" -- Title of the menu displayed in the banner.
+Config_subtile = "Interaction" -- Subtitle of the menu.
+Config_TextureDictionary = "commonmenu" -- Name of the YTD file containing your assets.
+Config_TextureName = "gradient_bgd" -- Name of the asset in the file.
 
---- Config Esthétique
+--- Aesthetic Config
 
-Config_mainColor = "~r~" -- Couleur utilisée dans les séparateurs.
-Config_RightLabel = "→→→" --- Chaine de caractères affichée sur le côté droit du bouton.
+Config_mainColor = "~r~" -- Color used in the separators.
+Config_RightLabel = "→→→" --- String displayed on the right side of the button.
 
---- Commande / Touches pour acceder au menu
+--- Command / Keys to access the menu
 
-Config_commandeName = "CoordsMenu2" -- Nom de la commande permettant d’ouvrir le menu.
-Config_keysOpenMenu = "F1" -- Touche permettant d’ouvrir le menu.
+Config_commandeName = "CoordsMenu2" -- Name of the command to open the menu.
+Config_keysOpenMenu = "F1" -- Key to open the menu.
